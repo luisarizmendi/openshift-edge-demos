9bc06f899f88be7c2c97abdfd70b5a3612442651
Luis Arizmendi <luis.ariz@gmail.com>
2024-08-02 02:06:47 +0200

    gitops

--- demos/ztp/tools/virtual-bmc/test-redfish.sh
+++ demos/ztp/tools/virtual-bmc/test-redfish.sh
@@ -7,3 +7,7 @@ curl http://localhost:8000/redfish/v1/Systems/
 
 # ...or power it off
 #curl -d '{"ResetType":"ForceOff"}' -H "Content-Type: application/json" -X POST http://localhost:8000/redfish/v1/Systems/b6c92bbb-1e87-4972-b17a-12def3948890/Actions/ComputerSystem.Reset
+
+
+# or insted virtualcd
+# curl -d '{"Image": "https://download.cirros-cloud.net/0.6.2/cirros-0.6.2-aarch64-disk.img", "Inserted": true}'      -H "Content-Type: application/json"      -X POST      http://localhost:8000/redfish/v1/Systems/b6c92bbb-1e87-4972-b17a-12def3948890/VirtualMedia/Cd/Actions/VirtualMedia.InsertMedia
